6ca804f53a10957f4ed9017bb7ff6c1910cff0aa
brentdenboer <brent@wittig.nl>
2025-04-23 09:34:48 +0200

    Update .config/zsh/08_prompt.zsh

--- dot_config/zsh/08_prompt.zsh
+++ dot_config/zsh/08_prompt.zsh
@@ -1,9 +1,4 @@
 # Prompt Configuration
 # --------------------
-autoload -Uz vcs_info
-precmd() {
-  vcs_info
-}
-zstyle ':vcs_info:git:*' formats '%F{red}%b%f%F{green}%c%F{yellow}%u%f '
-zstyle ':vcs_info:git:*' actionformats '%F{red}%b%f (%F{yellow}%a%f)%F{green}%c%F{yellow}%u%f '
-PROMPT='%F{blue}%~%f${vcs_info_msg_0_}$'
+eval "$(starship init zsh)"
+
